4cab25471065ca7295404aa018bcb201593903eb
seongjun1011 <sj297916@gmail.com>
2026-03-19 16:27:28 +0900

    View 속성, 리스너, 기본위젯의 다양한 속성들

--- app/src/main/java/kr/ac/kopo/buttontest/MainActivity.java
+++ app/src/main/java/kr/ac/kopo/buttontest/MainActivity.java
@@ -1,24 +1,38 @@
 package kr.ac.kopo.buttontest;
 
 import android.os.Bundle;
-
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.widget.EditText;
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText edit1;
+    TextView text1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+//        setContentView(R.layout.activity_main2);
+//
+//        Button btn1 = findViewById(R.id.btn1);
+//        edit1 = findViewById(R.id.edit1);
+//        text1 = findViewById(R.id.text1);
+
+//        btn1.setOnClickListener(btnListener);
     }
+
+    View.OnClickListener btnListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+//            String name = edit1.getText().toString();
+//            Toast.makeText(getApplicationContext(), name +"님 환영합니다.", Toast.LENGTH_LONG).show();
+        }
+    };
 }
